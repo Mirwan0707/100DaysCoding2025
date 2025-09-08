e9ecf58fc1651287869f0dfe6fca54cc6e494e71
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-08 14:27:49 +0800

    Day1.java

---
+++ Day1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        String nama = "Mirwan";
+        System.out.println("Nama :" + nama);
+    }
+    
+}
