d227b6813c687314e7e9f21b5bf943699b8afce9
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-09 21:01:29 +0800

    Day2.java

--- Day2.java
+++ Day2.java
@@ -1,9 +1,8 @@
-ublic class Main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("hay");
-        System.out.print("perkenalkan ");
+        System.out.println("Mirwam");
         System.out.printf("nama saya %s%n berumur %d%n, ", "Mirwan", 18);
-        
+          System.out.print("hay");
     }
     
 }
