d40fafee99a198c71d96dde595d49e8f7613ccc9
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-11 21:23:07 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,10 @@
+public class Main {
+public static void main(String[] args) {
+    
+System.out.println("hay\tperkenalkan nama saya mirwan\n hobi saya voli \n S\bsaya telah menghapus satu karakter sebelumnya ");
+	
+
+  
+}
+
+}
