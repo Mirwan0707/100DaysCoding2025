0a648bdbeaad344f1aa12327f485e7328a953b45
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-12 18:15:41 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,14 @@
+public class main {
+public static void main(String[] args) {
+		byte a = 10 ;
+		short b = 70 ;
+		int c = 5000 ;
+		long d = 700000L;
+		
+		System.out.println("byte a ="+ a );
+		System.out.println("short b ="+ b);
+		System.out.println("int c =" + c);
+		System.out.println("long d ="+ d);
+		
+}
+}
