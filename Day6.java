8b5ed536f70983a624374829591b02e3a2e88865
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-13 20:04:59 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class main {
+public static void main(String[] args) {
+		double a = 3.333333 ;  
+		float b = 7.777f;
+		
+		System.out.println("double ="+ a );
+		System.out.println("float ="+ b );
+		
+}
+}
