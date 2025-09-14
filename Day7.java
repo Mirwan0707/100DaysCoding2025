d15c2ad7bd304e4db8e70e6bec33450a05d225b3
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-14 21:01:53 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+ublic class main {
+public static void main(String[] args) {
+		char  Nilai  = 'A' ;  
+		boolean Lulus = true ;
+		
+		System.out.println("Nilai = "+ Nilai);
+		System.out.println(" Apakah Lulus ? "+ Lulus);
+		
+}
+}
