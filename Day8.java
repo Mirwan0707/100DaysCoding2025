97b4f5fc4b62ef46fdef22e8fe673cfabe75ebac
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-15 21:12:17 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		String nama = "Mirwan";
+		String Kalimat = "Belajar coding";
+	
+		System.out.println("Halo nama saya " + nama);
+        System.out.println("saya " + Kalimat);
+        
+		}
+	
+	
+}
