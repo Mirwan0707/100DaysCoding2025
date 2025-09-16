755e908d813bec4e44226d955f761764c2cd1e95
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-16 23:25:02 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		String nama;
+		int tabungan;
+		
+	    nama = "Mirwan";
+	    System.out.println("nama awal "+ nama);
+	    nama = "reski";    
+	System.out.println("nama update "+ nama);
+		tabungan = 10000;
+	System.out.println("tabungan awal "+tabungan);
+		tabungan = 15000;
+System.out.println("tabungan update "+tabungan);
+
+
+	}
+	
+	
+}
