caad2fcfccedbe820c97a3c89493b6367135067d
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-17 20:05:53 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int umur = 18;
+		final int variable = 20;
+		
+		System.out.println("umur saya "+ umur);
+		System.out.println("finalnya "+ variable);
+	}
+}
