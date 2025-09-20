b864bd647eee3e42a124e7a9d52ba137ecf99c3e
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-20 20:25:58 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		String nama = "Mirwan";
+		int Umur = 18;
+		double ipk = 3.87;
+	
+	System.out.println("nama : " + nama);
+	System.out.print("Umur saya : " + Umur);
+	
+	System.out.printf("\nnama = %s,  umur = %d,  ipk = %f" , nama, Umur, ipk);
+	}
+}
