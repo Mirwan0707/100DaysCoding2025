e4eabd2f2a6b773dfbc1ff1d98a9d071e4cd28a1
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-22 22:34:49 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+	    
+	    //contoh penjumlahan aritmatika
+	System.out.println("Ini contoh penjumlahan");
+			int a = 10;
+		int b = 5;
+		int c = a+b;
+		System.out.println("10 + 5 = "+ c);
+		
+		//contoh pengurangan
+System.out.println("\nini contoh pengurangan");
+		int d = 8;
+		int e = 2;
+		int f = d - e;
+		System.out.println("8 - 2 = "+ f);
+		
+	}
+}
