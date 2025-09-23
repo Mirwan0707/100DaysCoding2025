26eb203dd88d9466507e648506f354a975b35703
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-23 15:02:16 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+	    
+	    //contoh perlalian dalam operasi aritmatika
+	System.out.println("Ini contoh perkalian");
+			int a = 2;
+		int b = 5;
+		int c = a * b;
+		System.out.println("2 * 5 = "+c);
+		
+		//contoh pembagian dalam operasi aritmatika
+System.out.println("\nini contoh pembagian");
+		int d = 8;
+		int e = 2;
+		int f = d / e;
+		System.out.println("8 / 2 = "+ f);
+		
+	}
+}
