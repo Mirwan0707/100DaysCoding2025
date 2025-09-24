e4f37f69b85dd539e31b74a587282dfb3028f162
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-24 19:24:33 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+	    
+    //contoh modulus atau sisa bagi dalam operasi aritmatika
+    
+	System.out.println("Ini contoh modulus");
+		int a = 5;
+		int b = 2;
+		int c = a % b;
+		System.out.println("2 % 5 = "+c);
+		
+	}
+}
