4971e5efdbaf9b9b15e5d952d23bb08f1d37b0e4
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-25 22:18:55 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+	    
+    byte a = 8;
+    short b = a;
+    
+    System.out.println("konversi dari byte keshort = "+ b);
+    
+     int c = 32;
+     long d = c;
+     System.out.println("konversi dari int ke long = "+ d);
+     
+
+    
+    
+	
+		
+	}
+}
