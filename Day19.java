42f2a60a2333258b128e4d9c792088f06e5818d6
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-26 20:17:58 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+
+	   //konversi tipe data besar ke kecil
+    
+     long c = 999992;
+      int d = (int) c ;
+     System.out.println("konversi longe ke int = "+ d);
+     
+
+    
+    
+	
+		
+	}
+}
