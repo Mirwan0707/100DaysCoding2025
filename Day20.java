18bbc2161cd77a550840e1e8e0f8bc880a029f35
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-27 21:54:37 +0800

    Day20.java

--- Day20.java
+++ Day20.java
@@ -2,16 +2,16 @@ public class Main {
 	public static void main(String[] args) {
 
  	byte a = 7;
-	 String aByte= String.valueOf(a);
-      System.out.println("byte jadi string : "+ aByte);
+	 String strA= a + "";
+      System.out.println("byte jadi string : "+ strA);
          
       short b = 27;
       String bShort = String.valueOf(b);
       System.out.println("short jadi string : "+ bShort);
       	
       int c = 77;
-      String cInteger= String.valueOf(c);
-      System.out.println("integer jadi string : "+ cInteger);
+      String str = Integer.toString(c);
+      System.out.println("integer jadi string : "+ str);
 		
        
 
