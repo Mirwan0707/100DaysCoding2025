8e244690c2f799ebfab2559d4df27c9d840de10d
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-27 12:56:06 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+
+ 	byte a = 7;
+	 String aByte= String.valueOf(a);
+      System.out.println("byte jadi string : "+ aByte);
+         
+      short b = 27;
+      String bShort = String.valueOf(b);
+      System.out.println("short jadi string : "+ bShort);
+      	
+      int c = 77;
+      String cInteger= String.valueOf(c);
+      System.out.println("integer jadi string : "+ cInteger);
+		
+       
+
+	}
+}
