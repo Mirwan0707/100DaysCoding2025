8af06968dd56184b7fcbf0441cbf003db74701f9
Mirwan0707 <mirwanirwan416@gmail.com>
2025-09-30 19:00:42 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		double panjang = 10.0;
+		double lebar = 5.0;
+		double luas = panjang*lebar;
+		
+		System.out.println("luas persegi panjang : "+ luas + "cm");
+	}
+}
