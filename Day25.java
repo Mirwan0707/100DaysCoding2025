83b755de86bcde3c75941580d8827040814ac65e
Mirwan0707 <mirwanirwan416@gmail.com>
2025-10-02 16:39:29 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 10;
+		// ++ itu tambah 1 
+		a++;
+	    System.out.println(a);
+	   
+	   //-- itu kurang 1
+	    int b = 5;
+	    b--;
+	    System.out.println(b);
+	}
+}
