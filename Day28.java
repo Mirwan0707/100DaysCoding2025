7772eb92db2cfef7cb16284ec7e083d3bcc05805
Mirwan0707 <mirwanirwan416@gmail.com>
2025-10-05 11:39:25 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		//operator perbandingan (== , !=)
+		
+//operator perbandingan == atau sama dengan
+		int a = 8;
+		int b = 5;
+		System.out.println("angka 8 dan 5, == (sama dengan) hasilnya :  " + (a==b));
+		
+		// operator perbandingan != atau tidak sama dengan 
+		int y = 8;
+		int d = 3;
+		System.out.println("angka 8 dan 3, != (tidak sama dengan ) hasilnya : " + (y != d));
+	}
+}
