8fda4d54ac8ec5e7a2bc8f81d4a72f1d0f5820e8
Mirwan0707 <mirwanirwan416@gmail.com>
2025-10-06 18:34:16 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		//operator perbandingan (< , >)
+	
+//operator perbandingan lebih kecil
+		int a = 8;
+		int b = 5;
+		System.out.println("apakah angka 8 lebih kecil dengan 5   :  " + (a<b));
+		
+		// operator perbandingan > lebih besar
+		int y = 8;
+		int d = 3;
+		System.out.println("apakah angka  8 lebih besar dengan 3  : " + (y > d));
+	}
+}
