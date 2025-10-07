f829db6a83884288680a941cecc32812685ccb6b
Mirwan0707 <mirwanirwan416@gmail.com>
2025-10-07 19:50:43 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		//operator perbandingan (< , >)
+	
+//operator perbandingan <= lebih kecil atau sama dengan 
+		int a = 10;
+		int b = 10;
+		System.out.println("apakah angka 10 lebih kecil atau sama  dengan 10  :  " + (a<=b));
+		
+		
+		// operator perbandingan >= lebih besar atau sama dengan 
+		int y = 8;
+		int d = 3;
+		System.out.println("apakah angka  8 lebih besar atau sama  dengan 3  : " + (y >=d));
+	}
+}
