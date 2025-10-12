2c00dfb4f4d4d387632de4bb6368c03a4ce5e664
Mirwan0707 <mirwanirwan416@gmail.com>
2025-10-12 19:02:22 +0800

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+   System.out.print("masukkan umur anda  : ");
+   int umur= s.nextInt();
+  
+         if (umur >= 18) {
+           System.out.println("anda sudah dewasa");
+   } 
+          
+	}
+}
