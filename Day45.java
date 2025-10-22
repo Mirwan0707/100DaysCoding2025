88cb77d77cb9524da934e7f0797c89e730d6f779
Mirwan0707 <mirwanirwan416@gmail.com>
2025-10-22 22:50:24 +0800

    Day45.java

---
+++ Day45.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("masukkan angka 1-7 :");
+		int hari =s.nextInt();
+		
+		switch (hari) {
+		   case 1 :
+	        	System.out.println("hari senin");
+	       	break;
+	       	
+	   	case 2 :
+	      	System.out.println("hari selasa");
+        	break;
+        	
+	    	case 3:
+	        	System.out.println("hari rabu");
+	        	break;
+	        	
+	    	case 4:
+        		System.out.println("hari kamis");
+	       	break ;
+		
+	    	case 5 :
+        		System.out.println("hari jumat");
+	    	break;
+        	case 6 :
+	        	System.out.println("hari sabtu");
+	        	break;
+		case 7 :
+		System.out.println("hari minggu");
+		break;
+		
+		default:
+	
+		System.out.println("angka tidak sesuai");
+		}
+	}
+	
+}
